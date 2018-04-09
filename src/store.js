4d9cd8112bfb6071abec67fd6221a9c810311880
Rizal Ibnu <muhamadrizalibnuabdulah@gmail.com>
2018-04-09 17:22:20 +0700

    Remove unused modules

--- src/store.js
+++ src/store.js
@@ -2,13 +2,11 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { loadingBarMiddleware } from 'react-redux-loading-bar';
 import rootReducer from './reducers';
 
 export default function configureStore(initialState = {}) {
   const middleware = composeWithDevTools(applyMiddleware(
     promise(),
-    loadingBarMiddleware(),
     thunk,
   ));
 
