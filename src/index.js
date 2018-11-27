47b7bb5f929ccd58ef50a7e9e4eda4cc9271d8e8
Muhamad Rizal Ibnu Abdulah <rizal.ibnu@kumparan.com>
2018-11-27 07:33:23 +0700

    Update service worker using default cra 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { ThemeProvider } from 'react-jss';
 import App from './components/App';
 import configureStore from './store';
 import ScrollToTop from './components/ScrollToTop';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const theme = {
   brandPrimary: '#1abedb',
@@ -47,4 +47,7 @@ if (module.hot) {
   });
 }
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
